0dd6225ce6c16d871825ff956e6cfdcbf99baba1
JacksonRankin117 <ran23008@byui.edu>
2025-10-04 19:16:39 -0600

    Cleaning up comments

--- 6_Pointers.cpp
+++ 6_Pointers.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-// --- Function Declarations ---
+// ********************************************** Function Declarations ************************************************
 void increment(int *ptr);     // function using pointer
 void printArray(int *arr, int size); // function with pointer to array
 
@@ -10,7 +10,7 @@ void printArray(int *arr, int size); // function with pointer to array
 int main() {
     cout << "=== Pointer Demonstration ===\n" << endl;
 
-    // --- Basic pointer usage ---
+    // Basic pointer usage 
     int a = 10;
     int *p = &a; // pointer to int
 
@@ -19,15 +19,15 @@ int main() {
     cout << "Pointer p (holds address of a): " << p << endl;
     cout << "Value pointed to by p (*p): " << *p << endl;
 
-    // --- Modifying value using pointer ---
+    // Modifying value using pointer
     *p = 20; // changes a through pointer
     cout << "New value of a after *p = 20: " << a << endl;
 
-    // --- Passing pointer to function ---
+    // Passing pointer to function
     increment(&a);
     cout << "Value of a after increment function: " << a << endl;
 
-    // --- Pointer and arrays ---
+    // Pointer and arrays
     int arr[5] = {1, 2, 3, 4, 5};
     cout << "\nArray values using pointer arithmetic:" << endl;
     for (int i = 0; i < 5; i++) {
@@ -38,7 +38,7 @@ int main() {
     cout << "\nPrinting array with function:" << endl;
     printArray(arr, 5);
 
-    // --- Dynamic memory allocation ---
+    // Dynamic memory allocation
     int *dynArr = new int[3]; // allocate array of size 3
     dynArr[0] = 100;
     dynArr[1] = 200;
@@ -55,9 +55,9 @@ int main() {
     return 0;
 }
 
-// --- Function Definitions ---
+// *********************************************** Function Definitions ************************************************
 void increment(int *ptr) {
-    (*ptr)++; // dereference pointer and increment value
+    (*ptr)++;
 }
 
 void printArray(int *arr, int size) {
